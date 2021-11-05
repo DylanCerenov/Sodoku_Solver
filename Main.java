e7298a52a3dc8ef1304697ce49fcdb85ff1ad680
Dylan Cerenov <dcerenov25@gmail.com>
2021-11-05 15:47:32 -0400

    Initial Upload
    First push, uploading files.

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("sodoku solver");
+
+        // 9x9 grids
+        // those grids are set up as 3x3 boxes
+
+        /*
+        - Every square has to contain a single number
+        - Only the numbers from 1 through to 9 can be used
+        - Each 3×3 box can only contain each number from 1 to 9 once
+        - Each vertical column can only contain each number from 1 to 9 once
+        - Each horizontal row can only contain each number from 1 to 9 once
+         */
+
+
+        Sodoku board = new Sodoku();
+
+
+
+
+    }
+}
---
+++ Sodoku.java
@@ -0,0 +1,38 @@
+import java.util.List;
+
+public class Sodoku
+{
+    // global variables
+    // list of boxes would be the puzzle
+    List<int[][]> board;
+
+    // constructor for no input
+    public Sodoku()
+    {
+        for (int i = 0; i < 9; i++)
+            board.add(new int[3][3]);
+    }
+
+    // constructor that accounts for input.
+
+
+
+    // method for checking each 9x9 box's validity
+
+
+    // method for checking an individual row's validity
+
+
+    // method for checking an individual column's validity
+
+
+    // method for printing out the board
+
+
+
+
+
+
+
+
+}
